7907fe6a5ecb36414d335c8281680a62bdacac08
Azan Adnan <azanbutt2311@gmail.com>
2024-02-12 09:43:24 +0000

    Feb 12, 2024, 2:43 PM

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {}
+}
